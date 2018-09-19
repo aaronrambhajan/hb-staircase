c94e1fcb72c78d3203dbba9f9a852ddb3cc96517
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-09-19 16:52:31 -0400

    [server] Cleaning up server file

--- server.js
+++ server.js
@@ -69,34 +69,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-/*
-// Set route path & initialize API
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello, World!' });
-});
-
-// Router configuration when we call /api
-app.use('/api', router);
-
-// Route to save the trial
-router.post('/response', (req, res) => {
-  const rpns = new Response();
-
-  const { subject, sound, intensity, trial, isCorrect, rt } = req.body;
-
-  rpns.subject = subject;
-  rpns.sound = sound;
-  rpns.intensity = intensity;
-  rpns.trial = trial;
-  rpns.isCorrect = isCorrect;
-  rpns.rt = rt;
-
-  rpns.save((err) => {
-    return err
-      ? res.json({ success: false, error: err })
-      : res.json({ success: true });
-  });
-});
-*/
-
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
