8a0a9d1059d8b79f9cc5c799acc41438014eb253
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-09-19 00:06:54 -0400

    [api] Changing server routes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import path from 'path';
 import mongoose from 'mongoose';
 import session from 'express-session';
 import connectMongo from 'connect-mongo';
@@ -33,13 +34,42 @@ app.use(
 );
 
 // set our port to either a predetermined port number if you have set it up, or 3001
-const API_PORT = process.env.API_PORT || 3001;
+const API_PORT = process.env.API_PORT || 5000;
 
 // now we should configure the API to use bodyParser and look for JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
+// serve static files from React App
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.post('/api/response', (req, res) => {
+  const rpns = new Response();
+
+  const { subject, sound, intensity, trial, isCorrect, rt } = req.body;
+
+  rpns.subject = subject;
+  rpns.sound = sound;
+  rpns.intensity = intensity;
+  rpns.trial = trial;
+  rpns.isCorrect = isCorrect;
+  rpns.rt = rt;
+
+  rpns.save((err) => {
+    return err
+      ? res.json({ success: false, error: err })
+      : res.json({ success: true });
+  });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match the one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+/*
 // Set route path & initialize API
 router.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
@@ -67,5 +97,6 @@ router.post('/response', (req, res) => {
       : res.json({ success: true });
   });
 });
+*/
 
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
